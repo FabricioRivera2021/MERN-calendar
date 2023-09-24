9d4e9511e081d4e2893bad767342cb7c29190bdb
unknown <fabricio.rivera2012@gmail.com>
2023-09-24 18:20:21 -0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import { Navigate, Route, Routes } from "react-router-dom"
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+
+
+export const AppRouter = () => {
+
+    const authStatus = 'authenticated';
+
+    return(
+        <Routes>
+            {
+                (authStatus === 'not-authenticated')
+                ? <Route path='/auth/*' element={ <LoginPage /> } />
+                : <Route path='/*' element={ <CalendarPage /> } />
+            }
+            
+            <Route path='/*' element={ <Navigate to='/auth/login' /> }/>
+        </Routes>
+    )
+}
